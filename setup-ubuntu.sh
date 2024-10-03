65679a4197cb20e194ea62ef844f00f829e3aca5
Null <pairmanxlr@gmail.com>
2024-10-03 15:02:17 +0800

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -13,7 +13,13 @@ sudo apt-get \
   libtool \
   pkg-config
 if [ -n "$CROSS_COMPILE" ]; then
-  apt-get \
-    --allow-remove-essential --allow-change-held-packages -y \
-    "gcc-$CROSS_COMPILE" "g++-$CROSS_COMPILE"
+  if [ "CROSS_COMPILE" = "aarch64-w64-mingw" ]; then
+    wget https://github.com/mstorsjo/llvm-mingw/releases/download/20241001/llvm-mingw-20241001-msvcrt-ubuntu-20.04-x86_64.tar.xz
+    tar -xf llvm-mingw-*.tar.xz
+    export PATH="$(realpath "llvm-mingw-*/"):$PATH"
+  else
+    apt-get \
+      --allow-remove-essential --allow-change-held-packages -y \
+      "gcc-$CROSS_COMPILE" "g++-$CROSS_COMPILE"
+  fi
 fi
