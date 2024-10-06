696bcd02ba48730ff3ab9bfe53f3bccdee45ac03
Null <pairmanxlr@gmail.com>
2024-10-06 12:16:46 +0800

    Update download.pl

--- download.pl
+++ download.pl
@@ -196,6 +196,7 @@ foreach my $mirror (@ARGV) {
   }
 }
 
+push @mirrors, 'https://fossies.org/linux/misc';
 #push @mirrors, 'http://mirror1.openwrt.org';
 push @mirrors, 'http://mirror2.openwrt.org/sources';
 push @mirrors, 'http://downloads.openwrt.org/sources';
