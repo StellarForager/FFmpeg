0d1e5c1a0a0cc8a669fa90b6e5bed9082233a782
Null <pairmanxlr@gmail.com>
2025-04-18 07:12:13 +0800

    update llvm-mingw

--- setup-dependencies.sh
+++ setup-dependencies.sh
@@ -10,7 +10,7 @@ elif grep -qE 'debian|ubuntu' /etc/os-release; then
     --allow-remove-essential -y install \
     autoconf automake build-essential curl gawk libtool pkg-config tar
   if [ "$CROSS_COMPILE_PKG_SUFFIX" = "mingw-w64-aarch64" ] && [ $is_x86 -eq 1 ]; then
-    wget https://github.com/mstorsjo/llvm-mingw/releases/download/20250402/llvm-mingw-20250402-msvcrt-ubuntu-20.04-x86_64.tar.xz
+    wget https://github.com/mstorsjo/llvm-mingw/releases/download/20250417/llvm-mingw-20250417-msvcrt-ubuntu-20.04-x86_64.tar.xz
     tar -xf llvm-mingw-*.tar.xz
     export PATH="$(realpath "llvm-mingw-*/"):$PATH"
   elif [ -n "$CROSS_COMPILE_PKG_SUFFIX" ]; then
